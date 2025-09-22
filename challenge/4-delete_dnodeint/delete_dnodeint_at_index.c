e35c431fef2ef768c44af8bfe10683aed3eaeb76
FontGuillaume <guillaume_font@orange.fr>
2025-09-22 11:36:24 +0200

    Réorganiser et simplifier la fonction delete_dnodeint_at_index pour améliorer la lisibilité

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,51 +2,13 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Delete a node at a specific index from a list
- *
- * @head: A pointer to the first element of a list
- * @index: The index of the node to delete
- *
- * Return: 1 on success, -1 on failure
- */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
-{
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	dlistint_t *current = *head;
-	unsigned int p;
-
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && current != NULL)
-	{
-		current = current->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (index == 0)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;#include "lists.h"
-#include <stdlib.h>
-
-/**
- * delete_dnodeint_at_index - Delete a node at a specific index from a list
- *
- * @head: A pointer to the first element of a list
- * @index: The index of the node to delete
- *
- * Return: 1 on success, -1 on failure
- */
+* delete_dnodeint_at_index - Delete a node at a specific index from a list
+*
+* @head: A pointer to the first element of a list
+* @index: The index of the node to delete
+*
+* Return: 1 on success, -1 on failure
+*/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *saved_head;
@@ -84,22 +46,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		(*head)->prev->next = (*head)->next;
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
-        free(*head);
-		*head = saved_head;
-	}
-	return (1);
-}
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		current->prev->next = current->next;
-		if (current->next)
-			current->next->prev = current->prev;
-		free(current);
+		free(*head);
 		*head = saved_head;
 	}
 	return (1);
